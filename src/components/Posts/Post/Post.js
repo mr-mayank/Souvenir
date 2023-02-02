b45f1d0d9dbafdf519b2f0273ea88728d2da6aaa
mr-mayank <dvpatel9966@gmail.com>
2023-02-03 01:53:35 +0530

    instant like

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardActions, CardContent, CardMedia, Typography, CardHeader, Avatar, IconButton } from '@mui/material';
 import {Delete} from '@mui/icons-material';
 import { red } from '@mui/material/colors';
@@ -10,15 +10,28 @@ import moment from 'moment';
 import { deletePost, likePost } from '../../../actions/posts';
 const Post = ({post, setCurrentId}) => {
     const dispatch = useDispatch();
-
+    const [likes, setLikes] = useState(post?.likes);
     const user = JSON.parse(localStorage.getItem('profile'));
+    const userId = user?.result.googleId || user?.result?._id;
+    const hasLikedPost = post?.likes?.find((like) => like === userId);
+  
+    const handleLike = async () => {
+      dispatch(likePost(post._id));
+  
+      if (hasLikedPost) {
+        setLikes(post.likes.filter((id) => id !== userId));
+      } else {
+        setLikes([...post.likes, userId]);
+      }
+    };
+    
     const Likes = () => {
-        if (post.likes.length > 0) {
-          return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+        if (likes.length > 0) {
+          return likes.find((like) => like === userId)
             ? (
-              <><FavoriteSharpIcon sx={{ color: red[500] }} />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+              <><FavoriteSharpIcon sx={{ color: red[500] }} />&nbsp;{likes.length > 2 ? `You and ${likes.length - 1} others` : `${likes.length} like${likes.length > 1 ? 's' : ''}` }</>
             ) : (
-              <><FavoriteBorderOutlinedIcon fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+              <><FavoriteBorderOutlinedIcon fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}</>
             );
         }
         return <><FavoriteBorderOutlinedIcon fontSize="small" />&nbsp;Like</>;
@@ -55,7 +68,7 @@ const Post = ({post, setCurrentId}) => {
                 <Typography variant="body" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions>
-        <IconButton aria-label="add to favorites" disabled={!user?.result} onClick={ () => dispatch(likePost(post._id)) }>
+        <IconButton aria-label="add to favorites" disabled={!user?.result} onClick={handleLike}>
           <Likes /> 
         </IconButton>
         {user?.result?.googleId === post?.creator || user?.result?._id === post?.creator ? (
